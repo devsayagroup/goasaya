0f0b0685cef14f28bbb2b78d6d985c84f61737ae
devsayagroup <devsayagroup@gmail.com>
2025-12-16 16:39:16 +0700

    finalize

--- lib/google.ts
+++ lib/google.ts
@@ -18,3 +18,20 @@ const auth = new google.auth.JWT({
 
 export const sheets = google.sheets({ version: "v4", auth });
 export const calendar = google.calendar({ version: "v3", auth });
+
+
+
+// import { google } from "googleapis";
+// import serviceAccount from "@/credentials/google-account-service.json";
+
+// const auth = new google.auth.JWT({
+//   email: serviceAccount.client_email,
+//   key: serviceAccount.private_key,
+//   scopes: [
+//   "https://www.googleapis.com/auth/spreadsheets",
+//   "https://www.googleapis.com/auth/calendar",
+// ]
+// });
+
+// export const sheets = google.sheets({ version: "v4", auth });
+// export const calendar = google.calendar({ version: "v3", auth });
