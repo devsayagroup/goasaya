1e68ab13e7fb6a63d70fa4d12fe0719fd2acd9c3
devsayagroup <devsayagroup@gmail.com>
2026-01-21 19:46:37 +0700

    revise menu page

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,15 +1,16 @@
-import Head from "../head";
+// import Head from "../head";
 import MenuPage from "@/components/pages/MenuPage";
 import type { Metadata } from "next";
 
 const baseUrl = "https://goasaya.com";
 
 export const metadata: Metadata = {
-  title: "Menu | GoaSaya",
+  title: "Menu | GOASAYA Asian Progressive Dining",
   description:
     "Explore the full GOASAYA culinary experience. From appetizers, mains, desserts, premium beverages, cocktails, and signature drinks.",
-  openGraph: {
-    title: "Menu | GoaSaya",
+  keywords:"fine dining menu, asian cuisine menu, tasting menu, goasaya dishes, luxury restaurant food, signature menu",
+    openGraph: {
+    title: "Menu | GOASAYA Asian Progressive Dining",
     description:
       "Explore the full GOASAYA culinary experience. From appetizers, mains, desserts, premium beverages, cocktails, and signature drinks.",
     url: `${baseUrl}/menu`,
@@ -32,17 +33,11 @@ export const metadata: Metadata = {
     images: [`${baseUrl}/images/og-menu.jpg`],
   },
 };
+
 export default function Menu() {
   return (
     <>
-        <Head 
-            title= "Menu | GOASAYA Asian Progressive Dining"
-            description= "Explore the GOASAYA menu — a curated selection of Asian progressive dishes, tasting menus, and signature creations. Every plate tells a story of flavor, craftsmanship, and elegance."
-            url="https://www.goasaya.com"
-            keywords="fine dining menu, asian cuisine menu, tasting menu, goasaya dishes, luxury restaurant food, signature menu"
-            image="https://www.goasaya.com/logo/09.jpg"
-        />
-        <MenuPage/>
+    <MenuPage/>
     </>
   );
 }
