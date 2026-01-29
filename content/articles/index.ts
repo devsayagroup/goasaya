284c74886ffa451ec85bec5e81d848c0f3c4a777
devsayagroup <devsayagroup@gmail.com>
2026-01-29 14:34:26 +0700

    add 2 new journal

--- content/articles/index.ts
+++ content/articles/index.ts
@@ -1,13 +1,9 @@
-import SpotFoto, {
-  meta as spotFotoMeta,
-} from "./instagrammable-photo-spots-goasaya.mdx";
-import KonsepGoa, {
-  meta as konsepGoaMeta,
-} from "./interior-concept-goasaya.mdx";
-import PilosopiGoa, {
-  meta as pilosopiGoaMeta,
-} from "./menu-philosophy-goasaya.mdx"
+import SpotFoto, {meta as spotFotoMeta} from "./instagrammable-photo-spots-goasaya.mdx";
+import KonsepGoa, { meta as konsepGoaMeta} from "./interior-concept-goasaya.mdx";
+import PilosopiGoa, {meta as pilosopiGoaMeta} from "./menu-philosophy-goasaya.mdx"
 import MustVisit, {meta as mustVisitMeta} from "./must-visit-pik-2-jakarta.mdx"
+import MustTry, {meta as mustTryMeta} from "./must-try-desert-goasaya.mdx"
+import Firework, {meta as fireworkMeta} from "./chinese-firework-goasaya.mdx"
 
 
 export const articles = [
@@ -30,5 +26,15 @@ export const articles = [
     slug: mustVisitMeta.slug,
     meta: mustVisitMeta,
     Content: MustVisit,
+  },
+   {
+    slug: mustTryMeta.slug,
+    meta: mustTryMeta,
+    Content: MustTry,
+  },
+  {
+    slug: fireworkMeta.slug,
+    meta: fireworkMeta,
+    Content: Firework,
   },
 ];
