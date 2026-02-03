42bcf4cd9ffe51ea4ae9a0a8cfa511f68df510ed
devsayagroup <devsayagroup@gmail.com>
2026-02-03 18:58:43 +0700

    revise event system v2

--- lib/getEventById.ts
+++
@@ -1,6 +0,0 @@
-import { events } from "@/lib/events";
-import { OpsEvent } from "@/types/ops";
-
-export function getEventById(eventId: string): OpsEvent | null {
-  return events.find(e => e.id === eventId) || null;
-}
