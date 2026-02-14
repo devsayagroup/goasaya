a8a5ad09992bf87b9243315a0010bf91bff287ae
devsayagroup <devsayagroup@gmail.com>
2026-02-14 11:29:23 +0700

    update modal and event

--- content/events/index.ts
+++ content/events/index.ts
@@ -50,9 +50,9 @@ import ClayHolder, {meta as clayHolderMeta} from "./clay-photo-holder.mdx"
 import PaintWine, {meta as paintWineMeta} from "./paint-and-wine.mdx"
 
 export const events = [
-  {slug: paintWineMeta.slug, meta: paintWineMeta, Content: PaintWine},
+  // {slug: paintWineMeta.slug, meta: paintWineMeta, Content: PaintWine},
   {slug: clayHolderMeta.slug, meta: clayHolderMeta, Content: ClayHolder},
   {slug: northPoleMeta.slug, meta: northPoleMeta, Content: NorthPole},
   {slug: cavePartyMeta.slug, meta: cavePartyMeta, Content: CaveParty},
-  {slug: arabianNightMeta.slug, meta: arabianNightMeta, Content: ArabianNight},
+  // {slug: arabianNightMeta.slug, meta: arabianNightMeta, Content: ArabianNight},
 ];
